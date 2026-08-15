d58d526a2fef67c2f1429d19e93b9e1ab82aa6a5
ReventePro User <nqairidriss@yahoo.fr>
2026-08-15 21:00:48 +0200

    Extend navbar to full width for better spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-[#111111] border-b border-gray-800 sticky top-0 z-50 w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           
           {/* 1. LOGO + DATETIME */}
